fb649291afeb4970003783cad6c1035d69af0457
ruohan.chen <crhan123@gmail.com>
2018-04-21 14:00:17 +0800

    完善 protocol

--- zhong_hong_hvac/protocol.py
+++ zhong_hong_hvac/protocol.py
@@ -16,6 +16,10 @@ class ChecksumError(Exception):
 
 class FuncCode(enum.Enum):
     STATUS = 0x50
+    CTL_POWER = 0x31
+    CTL_TEMP = 0x32
+    CTL_MODE = 0x33
+    CTL_FAN_SPEED = 0x34
 
 
 class CtlStatus(enum.Enum):
@@ -28,7 +32,10 @@ class CtlStatus(enum.Enum):
 class StatusSwitch(enum.Enum):
     ON = 0x01
     OFF = 0x00
-    OFF2 = 0x02
+
+    @classmethod
+    def new_status_switch(cls, value):
+        return cls(value % 2)
 
 
 class StatusMode(enum.Enum):
@@ -44,10 +51,23 @@ class StatusFanSpeed(enum.Enum):
     LOW = 0x04
 
 
-def validate(data):
-    payload_lenth = len(data) - 1
-    check_sum = sum(struct.unpack('B' * payload_lenth, data[:-1])) % 256
-    return check_sum == struct.unpack('B', data[-1:])[0]
+STATUS_PAYLOAD_LEN = 10
+STATUS_ONLINE_PAYLOAD_LEN = 3
+AC_ADDR_LEN = 2
+
+
+def validate(data_frame):
+    """checksum of data frame."""
+    header = Header.get_header_from_frame(data_frame)
+    if not header.is_valid:
+        return False
+
+    data_checksum = sum(
+        struct.unpack('B' * header.checksum_position,
+                      data_frame[:header.checksum_position])) % 256
+    pos = header.checksum_position
+    data_checksum = struct.unpack('B', data_frame[pos:pos + 1])[0]
+    return data_checksum == data_checksum
 
 
 @attr.s
@@ -63,9 +83,7 @@ class ZhongHongDataStruct:
 
     @staticmethod
     def _sum(init, element):
-        if isinstance(element, enum.Enum):
-            element = element.value
-        return init + element
+        return init + ZhongHongDataStruct._to_int(element)
 
     @property
     def checksum(self):
@@ -84,6 +102,25 @@ class Header(ZhongHongDataStruct):
     ac_num = attr.ib()
 
     @property
+    def is_valid(self):
+        try:
+            self.check()
+        except ValueError:
+            logger.debug("header not valid: %s", self.export())
+            return False
+        return True
+
+    def check(self):
+        self.func_code
+        self.ctl_code
+
+    @classmethod
+    def get_header_from_frame(cls, data_frame):
+        if len(data_frame) < 4:
+            return None
+        return cls(*struct.unpack("BBBB", data_frame[:4]))
+
+    @property
     def func_code(self):
         return FuncCode(self._func_code)
 
@@ -91,11 +128,51 @@ class Header(ZhongHongDataStruct):
     def ctl_code(self):
         if self.func_code == FuncCode.STATUS:
             return CtlStatus(self._ctl_code)
+        elif self.func_code == FuncCode.CTL_POWER:
+            return StatusSwitch(self._ctl_code)
+        elif self.func_code == FuncCode.CTL_TEMP:
+            return self._ctl_code
+        elif self.func_code == FuncCode.CTL_FAN_SPEED:
+            return StatusFanSpeed(self._ctl_code)
         return None
 
     def __str__(self):
         return "Header: gw_addr %s, func: %s, ctl: %s, ac_num: %s" % (
             self.gw_addr, self.func_code, self.ctl_code, self.ac_num)
+
+    def is_status_update(self):
+        if self.func_code != FuncCode.STATUS:
+            return False
+        if self.ctl_code not in (CtlStatus.ALL, CtlStatus.ONE,
+                                 CtlStatus.MULTI):
+            return False
+        return True
+
+    @property
+    def length(self):
+        return 4
+
+    @property
+    def payload_length(self):
+        if self.func_code == FuncCode.STATUS:
+            if self.ctl_code in (CtlStatus.ONE, CtlStatus.MULTI,
+                                 CtlStatus.ALL):
+                payload_length = STATUS_PAYLOAD_LEN * self.ac_num
+            elif self.ctl_code in (CtlStatus.ONLINE):
+                payload_length = STATUS_ONLINE_PAYLOAD_LEN * self.ac_num
+            else:
+                raise Exception("unknown ctrl code: %s", self.header.export())
+        elif self.func_code in (FuncCode.CTL_POWER, FuncCode.CTL_TEMP,
+                                FuncCode.CTL_MODE, FuncCode.CTL_FAN_SPEED):
+            payload_length = AcAddr * self.ac_num
+        else:
+            raise Exception("unknown func code: %s", self.header.export())
+
+        return payload_length
+
+    @property
+    def checksum_position(self):
+        return self.length + self.payload_length
 
 
 @attr.s(slots=True, hash=True)
@@ -111,10 +188,10 @@ class AcAddr(ZhongHongDataStruct):
 class AcStatus(ZhongHongDataStruct):
     addr_out = attr.ib()
     addr_in = attr.ib()
-    _switch_status = attr.ib()
+    switch_status = attr.ib(converter=StatusSwitch.new_status_switch)
     target_temperature = attr.ib()
-    _mode = attr.ib()
-    _fan_speed = attr.ib()
+    mode = attr.ib(converter=StatusMode)
+    fan_speed = attr.ib(converter=StatusFanSpeed)
     room_temperature = attr.ib()
     error_code = attr.ib()
     padding1 = attr.ib()
@@ -123,18 +200,6 @@ class AcStatus(ZhongHongDataStruct):
     @property
     def ac_addr(self):
         return AcAddr(self.addr_out, self.addr_in)
-
-    @property
-    def switch_status(self):
-        return StatusSwitch(self._switch_status)
-
-    @property
-    def mode(self):
-        return StatusMode(self._mode)
-
-    @property
-    def fan_speed(self):
-        return StatusFanSpeed(self._fan_speed)
 
     def __str__(self):
         return "AC %s-%s power %s, mode %s, speed %s, target_temp %s, room_temp %s" % (
@@ -148,12 +213,51 @@ class AcData(object):
     payload = attr.ib(
         attr.Factory(collections.deque),
         init=False)  # type: List[ZhongHongDataStruct]
+    request = attr.ib(True)
 
     def add(self, data):
         self.payload.append(data)
 
     def __str__(self):
         return '\n'.join([str(self.header)] + [str(x) for x in self.payload])
+
+    @property
+    def length(self):
+        header_length = self.header.length
+        checksum_length = 1
+
+        if self.func_code == FuncCode.STATUS:
+            if self.ctl_code in (CtlStatus.ONE, CtlStatus.MULTI,
+                                 CtlStatus.ALL):
+                payload_length = STATUS_PAYLOAD_LEN * self.ac_num
+            elif self.ctl_code in (CtlStatus.ONLINE):
+                payload_length = STATUS_ONLINE_PAYLOAD_LEN * self.ac_num
+            else:
+                raise Exception("unknown ctrl code: %s", self.header.export())
+        elif self.func_code in (FuncCode.CTL_POWER, FuncCode.CTL_TEMP,
+                                FuncCode.CTL_MODE, FuncCode.CTL_FAN_SPEED):
+            payload_length = AcAddr * self.ac_num
+        else:
+            raise Exception("unknown func code: %s", self.header.export())
+
+        return header_length + checksum_length + payload_length
+
+    @property
+    def ac_num(self):
+        return self.header.ac_num
+
+    @property
+    def func_code(self):
+        return self.header.func_code
+
+    @property
+    def ctl_code(self):
+        return self.header.ctl_code
+
+    @property
+    def is_request(self):
+        '''Is this data a Request or Response.'''
+        return self.request
 
     @property
     def checksum(self):
@@ -170,17 +274,55 @@ class AcData(object):
                          for x in self.payload] + [self.bin_checksum])
 
 
-def parse_data(data):
-    if not validate(data):
+def get_data_frame(data):
+    """find frame in raw data.
+
+    Arguments:
+        data {bytes} -- raw bytes read from wire
+
+    Yields:
+        {bytes} -- a valid frame (checksum checked)
+
+    Returns:
+        None -- [description]
+    """
+    while data:
+        try:
+            if len(data) <= 5:
+                return None
+            header = Header.get_header_from_frame(data)
+            header.check()
+
+        except ValueError as e:
+            logger.error("header code unknown: %s", data[:4], exc_info=e)
+            data = data[1:]
+            continue
+
+        payload_length = header.payload_length
+        total_length = header.length + payload_length + 1
+        if len(data) < total_length:
+            logger.error("date length not enough")
+            return None
+
+        date_frame = data[:total_length]
+        if validate(date_frame):
+            yield date_frame
+        else:
+            logger.error("checksum error and drop this frame: %s", date_frame)
+        data = data[total_length:]
+
+
+def parse_data(data_frame):
+    if not validate(data_frame):
         raise ChecksumError("checksum error")
 
-    ac_data = AcData()
-    ac_data.header = header = Header(*struct.unpack("BBBB", data[:4]))
+    ac_data = AcData(request=False)
+    ac_data.header = header = Header.get_header_from_frame(data_frame)
     logger.debug(str(header))
 
     if header.func_code == FuncCode.STATUS:
         if header.ctl_code == CtlStatus.ONE:
-            ac_status = AcStatus(*struct.unpack('B' * 10, data[4:14]))
+            ac_status = AcStatus(*struct.unpack('B' * 10, data_frame[4:14]))
             logger.debug(ac_status.export())
             logger.debug(str(ac_status))
             ac_data.add(ac_status)
@@ -189,7 +331,7 @@ def parse_data(data):
                 start = 4 + idx * 10
                 end = 4 + (idx + 1) * 10
                 ac_data.add(
-                    AcStatus(*struct.unpack('B' * 10, data[start:end])))
+                    AcStatus(*struct.unpack('B' * 10, data_frame[start:end])))
         else:
             raise TypeError("not support type: %s" % header)
     else:

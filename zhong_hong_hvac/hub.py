807301613c5e6715a14b035744cd2d6a738bd5ce
ruohan.chen <crhan123@gmail.com>
2018-05-28 21:21:07 +0800

    version bump to 1.0.8
    - bugfix for hub creating socket

--- zhong_hong_hvac/hub.py
+++ zhong_hong_hvac/hub.py
@@ -32,7 +32,7 @@ class ZhongHongGateway:
     def __get_socket(self) -> socket.socket:
         logger.debug("Opening socket to (%s, %s)", self.ip_addr, self.port)
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        if platform == ('linux', 'linux2'):
+        if platform in ('linux', 'linux2'):
             s.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPIDLE, 1)  # pylint: disable=E1101
         if platform in ('darwin', 'linux', 'linux2'):
             s.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
